8f7df5912f17573ec05bae52794bc8585839c436
JongKing <hynb14@gmail.com>
2023-04-15 20:14:10 +0900

    composable, preview, modifier 학습

--- app/src/main/java/com/example/adroidpractice/MainActivity.kt
+++ app/src/main/java/com/example/adroidpractice/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.adroidpractice.ui.theme.AdroidPracticeTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,18 +28,23 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// Composable는 아무것도 반환할 수 없다.
+// xml 안쓰고 compose로 선언형 프로그래밍 쌉가능
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+     Surface(color = Color.Magenta){
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = Modifier.padding(24.dp)
+            // Modifier로 Composable를 강화 or 장식 가능
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AdroidPracticeTheme {
-        Greeting("Android")
+        Greeting("Jongking")
     }
 }
