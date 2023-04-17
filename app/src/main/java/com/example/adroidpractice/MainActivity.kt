50c19d67142326632adc2e79372601f7548c83fd
jongseok-oh <hynb14@gmail.com>
2023-04-17 16:34:05 +0900

    Image 추가, 전역 string, text padding

--- app/src/main/java/com/example/adroidpractice/MainActivity.kt
+++ app/src/main/java/com/example/adroidpractice/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.adroidpractice
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,7 +30,10 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background)
                 {
-                    BirthdayGreetingWithText(message = "종석아 생일 축하해 ^^7", from = "김치찌개")
+                    BirthdayGreetingWithText(
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text)
+                    )
                 }
             }
         }
@@ -48,15 +55,38 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun BirthdayGreetingWithText(message : String, from : String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = message,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = Modifier.padding(top = 16.dp)
         )
         Text(
             text = "from : $from",
-            fontSize = 36.sp
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .padding(end = 16.dp)
+                .align(alignment = Alignment.End)
         )
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message : String, from : String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+
+    Box{
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
     }
 }
 
@@ -65,6 +95,7 @@ fun BirthdayGreetingWithText(message : String, from : String, modifier: Modifier
 @Composable
 fun DefaultPreview() {
     AdroidPracticeTheme {
-        BirthdayGreetingWithText("종석아 생일 축하해 ^^7", "김치찌개")
+        //BirthdayGreetingWithText("종석아 생일 축하해 ^^7", "김치찌개")
+        BirthdayGreetingWithImage("종석아 생일 축하해 ^^7", "김치찌개")
     }
 }
